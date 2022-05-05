40c0d88ad4339d9369506abfac21ae6ae3d4ec4e
patrickga <patrick.g.alvarez@gmail.com>
2022-05-05 18:24:41 -0500

    Adds images for my projects in the portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,18 @@ import { Container } from "react-bootstrap";
 const Portfolio = (props) => {
     return (
         <Container>
-            <h1 className="text-center mt-5">Projects:</h1>
+            <h1 className="text-center mt-3">Projects:</h1>
+            <div className="row mt-4" id="project">
+                <div className="col-sm">
+                    <img src="/homiequest.png" alt="A robot standing on the street with cars heading towards it" />
+                </div>
+                <div className="col-sm">
+                 <img src="/spellbook.png" alt="" />
+                </div>
+                <div className="col-sm">
+                    <img src="/happy-hour.png" alt="" />
+                </div>
+            </div>
         </Container>
     )
 }
